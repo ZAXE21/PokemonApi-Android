d6ed9b4a3197f289e1e7c3672d7f128bf455914f
Alejandro Casado <alejandro.casado@imbox.me>
2023-01-30 21:05:16 +0100

    no message

--- PokemonApi/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/pokemonapi/databinding/ActivityMainBinding.java
+++ PokemonApi/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/pokemonapi/databinding/ActivityMainBinding.java
@@ -4,20 +4,32 @@ package com.example.pokemonapi.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.pokemonapi.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityMainBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityMainBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final ProgressBar progressBar;
+
+  @NonNull
+  public final ConstraintLayout root;
+
+  private ActivityMainBinding(@NonNull ConstraintLayout rootView, @NonNull ProgressBar progressBar,
+      @NonNull ConstraintLayout root) {
     this.rootView = rootView;
+    this.progressBar = progressBar;
+    this.root = root;
   }
 
   @Override
@@ -43,10 +55,21 @@ public final class ActivityMainBinding implements ViewBinding {
 
   @NonNull
   public static ActivityMainBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.progressBar;
+      ProgressBar progressBar = ViewBindings.findChildViewById(rootView, id);
+      if (progressBar == null) {
+        break missingId;
+      }
 
-    return new ActivityMainBinding((ConstraintLayout) rootView);
+      ConstraintLayout root = (ConstraintLayout) rootView;
+
+      return new ActivityMainBinding((ConstraintLayout) rootView, progressBar, root);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
